2d849cc91394d7959c38ffd8e75302865db43310
Jeremy Dorn <jeremy@jeremydorn.com>
2012-05-14 22:29:17 -0700

    Added directory option for all cache operations to store files in a specific subdirectory of the $cacheDir.  This is useful for grouping data by type.

--- FileSystemCache.php
+++ FileSystemCache.php
@@ -8,11 +8,17 @@ class FileSystemCache {
 	 * @param mixed $data The data to store (will be serialized before storing)
 	 * @param int $ttl The number of seconds until the cache expires.  Null means it doesn't expire.
 	 */
-	public static function store($key, $data, $ttl=null) {
+	public static function store($key, $data, $directory = null, $ttl=null) {		
+		if(is_numeric($directory)) {
+			$ttl = $directory;
+			$directory = null;
+		}
+		
 		if(!file_exists(self::$cacheDir.'/')) mkdir(self::$cacheDir);
+		if($directory && !file_exists(self::$cacheDir.'/'.$directory.'/')) mkdir(self::$cacheDir.'/'.$directory);
 		
 		$data = new FileSystemCacheValue($key,$data,$ttl);
-		$filename = self::getFileName($key);
+		$filename = self::getFileName($key,$directory);
 		
 		file_put_contents($filename,serialize($data));
 	}
@@ -23,8 +29,13 @@ class FileSystemCache {
 	 * @param int $newer_than If passed, only return if the cached value was created after this time
 	 * @return mixed The cached data or FALSE if not found
 	 */
-	public static function retrieve($key, $newer_than=false) {
-		$filename = self::getFileName($key);
+	public static function retrieve($key, $directory=null, $newer_than=false) {	
+		if(is_numeric($directory)) {
+			$newer_than = $directory;
+			$directory = null;
+		}
+		
+		$filename = self::getFileName($key, $directory);
 		if(file_exists($filename)) {
 			$data = unserialize(file_get_contents($filename));
 			
@@ -50,22 +61,22 @@ class FileSystemCache {
 	 * Invalidate a specific cache key (or entire cache)
 	 * @param String $key The cache key to invalidate or null to invalidate entire cache
 	 */
-	public static function invalidate($key=null) {
+	public static function invalidate($key=null,$directory=null) {
 		//if invalidating single cache entry
 		if($key) {
-			$filename = self::getFileName($key);
+			$filename = self::getFileName($key,$directory);
 			if(file_exists($filename)) {
 				unlink($filename);
 			}
 		}
-		//if invalidating entire cache
+		//if invalidating entire directory
 		else {
-			array_map("unlink", glob(self::$cacheDir.'/*.cache'));
+			array_map("unlink", glob(self::$cacheDir.'/'.($directory? $directory.'/':'').'*.cache'));
 		}
 	}
 	
-	protected static function getFileName($key) {
-		return self::$cacheDir.'/'.md5($key).'.cache';
+	protected static function getFileName($key, $directory = null) {		
+		return self::$cacheDir.'/'.($directory? $directory.'/' : '').md5($key).'.cache';
 	}
 }
 
